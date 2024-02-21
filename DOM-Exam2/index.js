5c762f7dec97e7372b5a998ff1aa0b57b315a3ff
Amit Deshmukh <shivpatil308@gmail.com>
2024-02-21 20:46:48 +0530

    changes added

--- DOM-Exam2/index.js
+++ DOM-Exam2/index.js
@@ -1,5 +1,4 @@
 
-
 let check=[]
 let form=document.querySelector("form");
 form.addEventListener("submit",(event)=>{
@@ -14,55 +13,12 @@ form.addEventListener("submit",(event)=>{
     //   console.log(obj)
 
      if(check.length==0){ 
-    axios.post("https://crudcrud.com/api/cae3a942dd244531b05857e2b3cce1e2/todos",obj)
+    axios.post("https://crudcrud.com/api/7bb65d43bf4c44529fb79fef889946c4/todos",obj)
     .then((res)=>{
         // console.log(res.data)
 
-        axios.get("https://crudcrud.com/api/cae3a942dd244531b05857e2b3cce1e2/todos")
-        .then((res)=>{
-            // console.log(res.data)
-            MyFun(res.data);
-            let star1=document.querySelector("#star1");
-            let star2=document.querySelector("#star2");
-            let star3=document.querySelector("#star3");
-            let star4=document.querySelector("#star4");
-            let star5=document.querySelector("#star5");
-    
-            let count1=0;
-            let count2=0;
-            let count3=0;
-            let count4=0;
-            let count5=0;
-            res.data.forEach((ele)=>{
-                // console.log(ele.rating)
-                if(ele.rating==1){
-                    count1++;
-                }
-                if(ele.rating==2){
-                    count2++;
-                }
-                if(ele.rating==3){
-                    count3++;
-                }
-                if(ele.rating==4){
-                    count4++;
-                }
-                if(ele.rating==5){
-                    count5++;
-                }
-                star1.textContent=count1;
-                star2.textContent=count2;
-                star3.textContent=count3;
-                star4.textContent=count4;
-                star5.textContent=count5;
-
-                document.querySelector("#name").value="";
-                document.querySelector("#rating").value=1;
-            })
-        })
-        .catch((err)=>{
-            console.log(err)
-        })
+           renderData()
+       
     })
     .then((err)=>{
         console.log(err)
@@ -71,65 +27,25 @@ form.addEventListener("submit",(event)=>{
 }
 else{
     // console.log(check[0])
-    axios.get(`https://crudcrud.com/api/cae3a942dd244531b05857e2b3cce1e2/todos/${check[0]}`)
+    axios.get(`https://crudcrud.com/api/7bb65d43bf4c44529fb79fef889946c4/todos/${check[0]}`)
     .then((res)=>{
         let editdata=(res.data);
 
         let name1=document.querySelector("#name").value;
         let rating1=document.querySelector("#rating").value;
-        axios.put(`https://crudcrud.com/api/cae3a942dd244531b05857e2b3cce1e2/todos/${check[0]}`,{
+        axios.put(`https://crudcrud.com/api/7bb65d43bf4c44529fb79fef889946c4/todos/${check[0]}`,{
             name:name1,
-            rating:rating1
+            rating:rating1,
+
         })
         .then((res)=>{
-            // console.log(res);
+            // console.log(res)
             check=[];
-
-            axios.get("https://crudcrud.com/api/cae3a942dd244531b05857e2b3cce1e2/todos")
-            .then((res)=>{
-                // console.log(res.data)
-                MyFun(res.data);
-                let star1=document.querySelector("#star1");
-                let star2=document.querySelector("#star2");
-                let star3=document.querySelector("#star3");
-                let star4=document.querySelector("#star4");
-                let star5=document.querySelector("#star5");
-        
-                let count1=0;
-                let count2=0;
-                let count3=0;
-                let count4=0;
-                let count5=0;
-                res.data.forEach((ele)=>{
-                    // console.log(ele.rating)
-                    if(ele.rating==1){
-                        count1++;
-                    }
-                    if(ele.rating==2){
-                        count2++;
-                    }
-                    if(ele.rating==3){
-                        count3++;
-                    }
-                    if(ele.rating==4){
-                        count4++;
-                    }
-                    if(ele.rating==5){
-                        count5++;
-                    }
-                    star1.textContent=count1;
-                    star2.textContent=count2;
-                    star3.textContent=count3;
-                    star4.textContent=count4;
-                    star5.textContent=count5;
-
-                    document.querySelector("#name").value="";
-                    document.querySelector("#rating").value="";
-                })
-            })
-            .catch((err)=>{
-                console.log(err)
-            })
+    // console.log(check)
+            
+      renderData()
+            
+            
         })
         .catch((err)=>{
             console.log(err)
@@ -139,6 +55,7 @@ else{
     .catch((err)=>{
         console.log(err)
     })
+    
 }
 
     
@@ -146,48 +63,8 @@ else{
 
 // ....................................................
 window.addEventListener("DOMContentLoaded",(e)=>{
-    axios.get("https://crudcrud.com/api/cae3a942dd244531b05857e2b3cce1e2/todos")
-    .then((res)=>{
-        // console.log(res.data)
-        MyFun(res.data);
-        let star1=document.querySelector("#star1");
-        let star2=document.querySelector("#star2");
-        let star3=document.querySelector("#star3");
-        let star4=document.querySelector("#star4");
-        let star5=document.querySelector("#star5");
-
-        let count1=0;
-        let count2=0;
-        let count3=0;
-        let count4=0;
-        let count5=0;
-        res.data.forEach((ele)=>{
-            // console.log(ele.rating)
-            if(ele.rating==1){
-                count1++;
-            }
-            if(ele.rating==2){
-                count2++;
-            }
-            if(ele.rating==3){
-                count3++;
-            }
-            if(ele.rating==4){
-                count4++;
-            }
-            if(ele.rating==5){
-                count5++;
-            }
-            star1.textContent=count1;
-            star2.textContent=count2;
-            star3.textContent=count3;
-            star4.textContent=count4;
-            star5.textContent=count5;
-        })
-    })
-    .catch((err)=>{
-        console.log(err)
-    })
+    
+    renderData()
 })
 // ..........................................................
 
@@ -220,10 +97,48 @@ ul.addEventListener("click",(e)=>{
 
     // DELETE BUTTON
     if(currentdel){
-        axios.delete(`https://crudcrud.com/api/cae3a942dd244531b05857e2b3cce1e2/todos/${getid}`)
+        axios.delete(`https://crudcrud.com/api/7bb65d43bf4c44529fb79fef889946c4/todos/${getid}`)
         .then((res)=>{
             // console.log(res)
-            axios.get("https://crudcrud.com/api/cae3a942dd244531b05857e2b3cce1e2/todos")
+            renderData()
+            
+                document.querySelector("#name").value="";
+                document.querySelector("#rating").value=1;
+                check=[];
+
+                
+           
+        })
+        .catch((err)=>{
+            console.log(err)
+        })
+    }
+
+    // EDIT FUNCTIONALITY
+    let currentEdit=e.target.classList.contains("editbtn");
+    let x4=x3.nextElementSibling;
+    // console.log(x4)
+    if(currentEdit){
+        // console.log(getid)
+        axios.get(`https://crudcrud.com/api/7bb65d43bf4c44529fb79fef889946c4/todos/${getid}`)
+        .then((res)=>{
+            let deta=res.data;
+            // console.log(deta._id)
+            check.push(deta._id);
+            document.querySelector("#name").value=deta.name;
+            document.querySelector("#rating").value=deta.rating;
+
+            
+        })
+        .catch((err)=>{
+            console.log(err)
+        })
+    }
+})
+
+
+function renderData(){
+     axios.get("https://crudcrud.com/api/7bb65d43bf4c44529fb79fef889946c4/todos")
             .then((res)=>{
                 // console.log(res.data)
                 MyFun(res.data);
@@ -255,40 +170,18 @@ ul.addEventListener("click",(e)=>{
                     if(ele.rating==5){
                         count5++;
                     }
-                    star1.textContent=count1;
-                    star2.textContent=count2;
-                    star3.textContent=count3;
-                    star4.textContent=count4;
-                    star5.textContent=count5;
                 })
+                star1.textContent=count1;
+                star2.textContent=count2;
+                star3.textContent=count3;
+                star4.textContent=count4;
+                star5.textContent=count5;
+
+                document.querySelector("#name").value="";
+                document.querySelector("#rating").value=1;
+                
             })
             .catch((err)=>{
                 console.log(err)
-            })
-        })
-        .catch((err)=>{
-            console.log(err)
-        })
-    }
-
-    // EDIT FUNCTIONALITY
-    let currentEdit=e.target.classList.contains("editbtn");
-    let x4=x3.nextElementSibling;
-    // console.log(x4)
-    if(currentEdit){
-        // console.log(getid)
-        axios.get(`https://crudcrud.com/api/cae3a942dd244531b05857e2b3cce1e2/todos/${getid}`)
-        .then((res)=>{
-            let deta=res.data;
-            // console.log(deta._id)
-            check.push(deta._id);
-            document.querySelector("#name").value=deta.name;
-            document.querySelector("#rating").value=deta.rating;
-
-            
-        })
-        .catch((err)=>{
-            console.log(err)
-        })
-    }
-})
+            });
+}
